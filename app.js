cc2f32dd00cbdfab1edd7f1d7fec8c83d931f7b0
Fajar Patappari <top.sick.red@gmail.com>
2017-12-05 21:11:49 +0700

    Add cookie parser and update vscode config

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const session = require('express-session')
-
+const cookieParser = require('cookie-parser')
+const checkLoginHandler = require('./helpers/authHelper').checkLoginHandler
 const app = express()
 // app.use(parse.json())
 app.set('view engine', 'ejs')
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(flashMessageHandler)
 app.use(session({
     secret : 'iloveyou'
 }))
-
 app.use('/auth',require('./routes/auth'))
-// app.use('/transaction',require('./routes/transaction'))
-app.use('/wishlist',require('./routes/wisht'))
-// app.use('/report',require('./routes/report'))
-// app.use('/budget',require('./routes/budget'))
+app.use('/transaction', checkLoginHandler, require('./routes/transaction'))
+app.use('/wishlist', checkLoginHandler, require('./routes/wisht'))
+app.use('/report', checkLoginHandler, require('./routes/report'))
+app.use('/budget', checkLoginHandler, require('./routes/budget'))
 app.use('/',function(req,res) {
     res.render('home',{
-        pageTitle : 'HOME'
+        pageTitle : 'Home'
     })
 })
 // app.use(errorHandler)
@@ -41,3 +43,16 @@ function errorHandler(err, req, res, next) {
       res.redirect(req.url)
     }
   }
+
+
+  function flashMessageHandler(req, res, next) {
+    res.flash = function(text) {
+      res.cookie('nosy_flash_message', text, { httpOnly: true })
+    }
+    if (req.cookies.nosy_flash_message) {
+      res.locals.flashMessage = req.cookies.nosy_flash_message
+      res.clearCookie('nosy_flash_message')
+    }
+    next()
+  }
+  
