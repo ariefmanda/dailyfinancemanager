c07c7a8da16e9105414c7cda6fb76ad96c876fcf
ariefmanda <arief.manda57@gmail.com>
2017-12-06 19:06:43 +0700

    transaction

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 const models = require('../models')
+const MonthHelper = require('../helpers/monthHelper')
 
 router.get('/', (req, res, next) => {
   models.User.findById(req.session.userId)
@@ -8,79 +9,298 @@ router.get('/', (req, res, next) => {
       user
         .getTransactions({ include: [models.Category] })
         .then(transactions => {
-            res.render('transactionList', {
-              user: user,
-              transactions: transactions,
-              pageTitle: 'List Transactions'
-            })
+          res.render('transactionList', {
+            user: user,
+            transactions: transactions,
+            pageTitle: 'List Transactions',
+            expenses : transactions.reduce((acc, transaction) =>{
+              return acc + transaction.price
+            },0)
+
+          })
         })
         .catch(next)
     })
     .catch(next)
 })
 router.get('/add', function(req, res, next) {
-  models.User.findById(req.session.userId).then(user=>{
-    user.getWishes().then(wishes=>{
-      models.Category.findAll().then(categories => {
-        res.render('transactionForm', {
-          wishes:wishes,
-          transaction: false,
-          categories: categories,
-          pageTitle: 'Transaction Add'
-        })
-      })
-    })
-  })
-})
-
-router.post('/add', function(req, res, next) {
-  models.User.findById(req.session.id)
+  models.User.findById(req.session.userId)
     .then(user => {
       user
-        .getBudget({
-          where: {
-            month: req.body.month,
-            year: req.body.year
-          }
-        })
-        .then(budget => {
-          // check budeget cukup ata
-          let priceNum = Number(req.body.price)
-          if (budget.expense + priceNum > budget.amount) {
-            res.flash('budget gak cukup')
-            res.redirect('/wish/add')
-          } else {
-            budget.expense += priceNum
-            budget
-              .save()
-              .then(() => {
-                user
-                  .addTransaction({
-                    name: req.body.name,
-                    price: priceNum,
-                    categoryId: req.body.categoryId
-                  })
-                  .then(() => {
-                    res.flash('OK masuk')
-                    res.redirect('/transaction')
-                    // user.getWish({
-                    //   where : {
-                    //     id: req.body.wishId
-                    //   }
-                    // }).then(wish =>{
-                    //   wish.fullfilled = true
-                    //   wish.save().then(()=>{
-                    //     res.redirect('/transaction')
-                    //   })
-                    // })
-                  })
+        .getWishes()
+        .then(wishes => {
+          models.Category.findAll()
+            .then(categories => {
+              res.render('transactionForm', {
+                wishes: wishes,
+                transaction: false,
+                categories: categories,
+                pageTitle: 'Transaction Add'
               })
-              .catch(next)
-          }
+            })
+            .catch(next)
         })
         .catch(next)
     })
     .catch(next)
+})
+
+router.post('/add', function(req, res, next) {
+  let convertDate = MonthHelper.ConvertDate(new Date())
+  let priceNum = Number(req.body.price)
+  models.User.findById(req.session.userId)
+    .then(user =>
+      Promise.all([
+        user,
+        models.Budget.findOne({
+          where: {
+            userId: user.id,
+            month: convertDate[1],
+            year: convertDate[2]
+          }
+        })
+      ])
+    )
+    .then(([user, budget]) => {
+      if (Number(budget.expense) + priceNum > budget.amount) {
+        res.flash('budget gak cukup')
+        res.redirect(`/wish/add?${req.body.name}`)
+      } else {
+        budget.expense = Number(budget.expense) + priceNum
+        return Promise.all([user, budget.save()])
+      }
+    })
+    .then(([user, budget]) => {
+      return Promise.all([
+        user,
+        budget,
+        models.Transaction.create(
+          {
+            name: req.body.name,
+            price: priceNum,
+            categoryId: req.body.categoryId,
+            userId: user.id
+          }
+        ),
+        models.Wish.update({
+          fullfilled:true
+        },{
+          where:{
+            userId:user.id,
+            id:req.body.wishId
+          }
+        })
+      ])
+    })
+    .then(([user,budget,transaction]) => {
+      res.redirect('/transaction')
+    })
+    .catch(next)
+
+  // models.User.findById(req.session.userId)
+  //   .then(user => {
+  //     let convertDate=MonthHelper.ConvertDate(new Date)
+  //     user.getBudget({
+  //         where: {
+  //           month: convertDate[1],
+  //           year: convertDate[2]
+  //         }
+  //       })
+  //       .then(budget => {
+  //         // check budeget cukup ata
+  //         let priceNum = Number(req.body.price)
+  //         if (budget.expense + priceNum > budget.amount) {
+  //           res.flash('budget gak cukup')
+  //           res.redirect(`/wish/add?${req.body.name}`)
+  //         } else {
+  //           budget.expense += priceNum
+  //           budget
+  //             .save()
+  //             .then(() => {
+  //               user
+  //                 .addTransaction({
+  //                   name: req.body.name,
+  //                   price: priceNum,
+  //                   categoryId: req.body.categoryId
+  //                 })
+  //                 .then(() => {
+  //                   res.flash('OK masuk')
+  //                   res.redirect('/transaction')
+  //                   if(Number(req.body.wishId)>0){
+  //                     user.getWish({
+  //                       where : {
+  //                         id: req.body.wishId
+  //                       }
+  //                     }).then(wish =>{
+  //                       wish.fullfilled = true
+  //                       wish.save().then(()=>{
+  //                         res.redirect('/transaction')
+  //                       })
+  //                     })
+  //                     .catch(next)
+  //                   }else{
+  //                     res.redirect('/transaction')
+  //                   }
+  //                 })
+  //                 .catch(next)
+  //             })
+  //             .catch(next)
+  //         }
+  //       })
+  //       .catch(next)
+  //   })
+  //   .catch(next)
+})
+
+router.get('/:id/edit', function(req, res, next) {
+  models.User.findById(req.session.userId)
+    .then(user => {
+      user
+        .getWishes()
+        .then(wishes => {
+          user
+            .getTransactions({
+              where: {
+                id: req.params.id
+              }
+            })
+            .then(transaction => {
+              models.Category.findAll()
+                .then(categories => {
+                  res.render('transactionForm', {
+                    wishes: wishes,
+                    transaction: transaction[0],
+                    categories: categories,
+                    pageTitle: 'Transaction Edit'
+                  })
+                })
+                .catch(next)
+            })
+            .catch(next)
+        })
+        .catch(next)
+    })
+    .catch(next)
+})
+
+
+
+router.post('/:id/edit', function(req, res, next) {
+  let convertDate = MonthHelper.ConvertDate(new Date())
+  models.User.findById(req.session.userId)
+    .then(user =>
+      Promise.all([
+        user,
+        models.Budget.findOne({
+          where: {
+            userId: user.id,
+            month: convertDate[1],
+            year: convertDate[2]
+          }
+        }),
+        models.Transaction.findOne({
+          where:{
+            id:req.params.id,
+            userId:user.id
+          }
+        })
+      ])
+    )
+    .then(([user, budget,transaction]) => {
+      let selisih = req.body.price - transaction.price
+      if ((Number(budget.expense) + selisih) > budget.amount) {
+        res.flash('budget gak cukup')
+        res.redirect(`/wish/add?${req.body.name}`)
+      } else {
+        budget.expense = Number(budget.expense) + selisih
+        return Promise.all([user, budget.save(),transaction])
+      }
+    })
+    .then(([user, budget,transaction])=>{
+      return Promise.all([models.Transaction.update({
+        name:req.body.name,
+        categoryId:req.body.categoryId,
+        price:req.body.price
+      },{
+        where:{
+          userId:user.id,
+          id:transaction.id
+        }
+      })])
+    })
+    .then(([user,budget,transaction]) => {
+      res.send('ok bro')
+    })
+    .catch(next)
+
+  // models.User.findById(req.session.userId)
+  //   .then(user => {
+  //     user
+  //       .getTransaction({
+  //         where: {
+  //           id: req.params.id
+  //         }
+  //       })
+  //       .then(transaction => {
+  //         let convertDate = MonthHelper.ConvertDate(transaction.created)
+  //         user
+  //           .getBudget({
+  //             where: {
+  //               month: convertDate[1],
+  //               year: convertDate[2]
+  //             }
+  //           })
+  //           .then(budget => {
+  //             let selisih = req.body.price - transaction.price
+  //             if (budget.expense + selisih > budget.amount) {
+  //               res.flash('budget gak cukup')
+  //               res.redirect(`/wish/add?${req.body.name}`)
+  //             } else {
+  //               budget.expense += selisih
+  //               budget
+  //                 .save()
+  //                 .then(() => {
+  //                   user
+  //                     .ransaction({
+  //                       name: req.body.name,
+  //                       price: priceNum,
+  //                       categoryId: req.body.categoryId
+  //                     })
+  //                     .then(() => {
+  //                       res.flash('OK masuk')
+  //                       res.redirect('/transaction')
+  //                       if (Number(req.body.wishId) > 0) {
+  //                         user
+  //                           .getWish({
+  //                             where: {
+  //                               id: req.body.wishId
+  //                             }
+  //                           })
+  //                           .then(wish => {
+  //                             wish.fullfilled = true
+  //                             wish.save().then(() => {
+  //                               res.redirect('/transaction')
+  //                             })
+  //                           })
+  //                           .catch(next)
+  //                       } else {
+  //                         res.redirect('/transaction')
+  //                       }
+  //                     })
+  //                     .catch(next)
+  //                 })
+  //                 .catch(next)
+  //             }
+  //           })
+  //           .catch(next)
+  //       })
+  //       .catch(next)
+  //   })
+  //   .catch(next)
+})
+
+router.get('/:id/delete', (req, re, next) => {
+  models.Transaction.destroy
 })
 
 module.exports = router
